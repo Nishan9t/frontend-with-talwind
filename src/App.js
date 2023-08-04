3089cc48e9a586e48532823f3b9b9e1141c398f1
Nishan9t <pandeynisha9t@gmail.com>
2023-08-04 12:08:21 +0530

    newsletter section is done

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import Header from './components/Header';
 import Banner from './components/Banner';
 import Experts from './components/Experts';
+import NewsLetter from './components/NewsLetter';
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <Header/>
       <Banner/>
       <Experts/>
+      <NewsLetter/>
     </div>
   );
 }
